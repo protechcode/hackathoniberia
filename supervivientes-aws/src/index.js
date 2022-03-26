d7ea41a569308a5c07b7a00084f57acf8577446d
protechcode <protechcode01@gmail.com>
2022-03-26 13:51:42 +0100

    react dom ready/router

--- supervivientes-aws/src/index.js
+++ supervivientes-aws/src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './component/Home';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="Home" element={<Home />} />
+      
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
