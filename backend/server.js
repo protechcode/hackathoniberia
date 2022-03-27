7d52da96e68778be5754156ee9472870ec9b742e
CRCode <c.romero.lozano1494@gmail.com>
2022-03-27 20:49:54 +0200

    connected to db

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 const express =require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const configFile = require('./config')
 
-const app = express();
-app.use(cors());
-// app.use(mongoose);
+const connection = configFile.dburi;
 
-const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>{console.log(`server up and running at: ${port}`)})
+mongoose
+.connect(connection)
+.then(() => {
+    const app = express();
+    app.use(cors());
+    
+
+    const port = process.env.PORT || 5000;
+    app.listen(port, ()=>{console.log(`server up and running at: ${port}`)})
+    
+})
+
+
+
