d93b3a284f591a245256bd9e824b12b4b7378e10
CRCode <c.romero.lozano1494@gmail.com>
2022-03-28 00:44:01 +0200

    Up for review

--- supervivientes-aws/src/component/Dondedormir.js
+++ supervivientes-aws/src/component/Dondedormir.js
@@ -2,12 +2,38 @@ import { React, useState } from 'react';
 import Navbar from "./Navbar";
 import axios from "axios";
 function Dondedormir(){
+    const [DormirArray, setDormirArray] = useState([]);
     async function getAlbergues(){
-        await axios.get("http://localhost:5000/api/albergues").then(res=>{console.log(res.data)})
+        await axios.get("http://localhost:5000/api/albergues").then(res => {
+            const data = res.data;
+            if (data) {
+                setDormirArray(data);
+                console.log(data)
+            } else {
+                console.log("No Data")
+            }
+        }).catch(e => { throw new Error })
     } 
+  
     return(
         <div>
             <Navbar/>
+            {DormirArray.map(({name, addr, tel})=>{
+                return(<div class="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl" >
+             
+
+                <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-white">{name}</h1>
+
+                <p class="text-gray-500 dark:text-gray-300">
+                   {addr} 
+                </p>
+                <p class="text-gray-500 dark:text-gray-300">
+                   {tel} 
+                </p>
+
+
+            </div>)
+            })}
             <h1>Donde Dormir</h1>
             <button onClick={()=>{getAlbergues()}}>Get</button>
         </div>
--- supervivientes-aws/src/component/Dondeducharse.js
+++ supervivientes-aws/src/component/Dondeducharse.js
@@ -1,13 +1,38 @@
-import React from "react";
+import { React, useState } from 'react';
 import Navbar from "./Navbar";
 import axios from "axios";
 function Dondeducharse(){
+    const [DuchasArray, setDuchasArray] = useState([]);
     async function getDuchas(){
-        await axios.get("http://localhost:5000/api/lavatorios").then(res=>{console.log(res.data)})
+        await axios.get("http://localhost:5000/api/lavatorios").then(res => {
+            const data = res.data;
+            if (data) {
+                setDuchasArray(data);
+                console.log(data)
+            } else {
+                console.log("No Data")
+            }
+        }).catch(e => { throw new Error })
     } 
     return(
        <div>
            <Navbar/>
+           {DuchasArray.map(({name, addr, tel})=>{
+                return(<div class="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl" >
+             
+
+                <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-white">{name}</h1>
+
+                <p class="text-gray-500 dark:text-gray-300">
+                   {addr} 
+                </p>
+                <p class="text-gray-500 dark:text-gray-300">
+                   {tel} 
+                </p>
+
+
+            </div>)
+            })}
             <h1>Donde Ducharse</h1>
             <button onClick={()=>{getDuchas()}}>Get</button>
        </div>
