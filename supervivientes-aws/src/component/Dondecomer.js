2edd10f9880d1588f6a3f6540bb44fcdf3177588
CRCode <c.romero.lozano1494@gmail.com>
2022-03-28 00:35:50 +0200

    cards for comedores

--- supervivientes-aws/src/component/Dondecomer.js
+++ supervivientes-aws/src/component/Dondecomer.js
@@ -1,17 +1,44 @@
-import React from "react";
+import { React, useState } from 'react';
 import Navbar from "./Navbar";
 
 import axios from "axios";
 
 function Dondecomer() {
-   async function getComedores(){
-        await axios.get("http://localhost:5000/api/comedores").then(res=>{console.log(res.data)})
+    const [ComedoresArray, setComedoresArray] = useState([]);
+    async function getComedores() {
+        await axios.get("http://localhost:5000/api/comedores").then(res => {
+            const data = res.data;
+            if (data) {
+                setComedoresArray(data);
+                console.log(data)
+            } else {
+                console.log("No Data")
+            }
+        }).catch(e => { throw new Error })
+
     }
     return (
         <div>
-            <Navbar/>
+            <Navbar />
+            {ComedoresArray.map(({name, addr, tel})=>{
+                return(<div class="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl" >
+             
+
+                <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-white">{name}</h1>
+
+                <p class="text-gray-500 dark:text-gray-300">
+                   {addr} 
+                </p>
+                <p class="text-gray-500 dark:text-gray-300">
+                   {tel} 
+                </p>
+
+
+            </div>)
+            })}
+            
             <h1>Donde Comer</h1>
-            <button onClick={()=>{getComedores()}}>Get</button>
+            <button onClick={() => { getComedores() }}>Get</button>
         </div>
 
     )
